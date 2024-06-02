ed29ef9c59192a2a4706049d921a37c39d843cb8
nocheol <qkdl60@gmail.com>
2024-06-03 01:57:49 +0900

    feat : alert 추가

--- qkdl60/src/utils/api.js
+++ qkdl60/src/utils/api.js
@@ -1,10 +1,27 @@
-const API_ENDPOINT = "https://q9d70f82kd.execute-api.ap-northeast-2.amazonaws.com/dev";
+const API_END_POINT = "https://q9d70f82kd.execute-api.ap-northeast-2.amazonaws.com/dev";
 
 export const api = {
-  fetchCats: (keyword) => {
-    return fetch(`${API_ENDPOINT}/api/cats/search?q=${keyword}`).then((res) => res.json());
+  fetchCats: (keyword, page = 1) => {
+    return fetch(`${API_END_POINT}/api/cats/search?q=${keyword}&page=${page}`).then((res) => res.json());
   },
   fetchRandom50: () => {
-    return fetch(`${API_ENDPOINT}/api/cats/random50`).then((res) => res.json());
+    return fetch(`${API_END_POINT}/api/cats/random50`)
+      .then((res) => {
+        if (!res.ok) throw new Error("not work ");
+        return res.json();
+      })
+      .catch((error) => {
+        window.alert("네트워크 연결에 실패했습니다\n 다시 시도해주세요😿😾");
+      });
+  },
+  fetchCat: (id) => {
+    return fetch(`${API_END_POINT}/api/cats/${id}`)
+      .then((res) => {
+        if (!res.ok) throw new Error("not work ");
+        return res.json();
+      })
+      .catch((error) => {
+        window.alert("네트워크 연결에 실패했습니다.\n 다시 시도해주세요😿😾");
+      });
   },
 };
